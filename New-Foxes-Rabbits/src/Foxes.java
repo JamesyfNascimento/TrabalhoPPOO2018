193d749550f164a42d40f41f234cb104bddca704
isabelamn07 <isabelamn07@gmail.com>
2018-12-02 18:42:45 -0200

    Arrumando Presa e Predador

--- New-Foxes-Rabbits/src/Foxes.java
+++ New-Foxes-Rabbits/src/Foxes.java
@@ -6,8 +6,19 @@
 
 /**
  *
- * @author isabela
+ * @author Anderson, Isabela, James
  */
-public class Foxes {
+public class Foxes extends Predador {
+    // Características compartilhadas por todos animais(static fields).
+    // Idade que um animal pode começar a procriar.
+    private static int BREEDING_AGE = 10;
+    // A idade limite de idade de um animal.
+    private static int MAX_AGE = 20;
+    // A probabilidade do animal de reproduzir
+    private static double BREEDING_PROBABILITY = 0.09;
+    // Número máximo de nascimentos.
+    private static int MAX_LITTER_SIZE = 3;
+    // Um gerador de um número aleatório para controlar a reprodução.
+    
     
 }
--- New-Foxes-Rabbits/src/Predador.java
+++ New-Foxes-Rabbits/src/Predador.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,31 @@
  *
  * @author isabela
  */
-public class Predador {
+public abstract class Predador {
+    
+    private static Random rand = new Random();
+    
+     // Idade dos animais
+    private int age; 
+    // Se o animal está vivo ou não
+    private boolean alive;
+
+    public Predador(int age, boolean alive) {
+        this.age = age;
+        this.alive = alive;
+        
+    }
+    
+    
+
+    public Predador() {
+        this.age = age;
+        this.alive = alive;
+    }
+
+    
+    
+    
+    
     
 }
