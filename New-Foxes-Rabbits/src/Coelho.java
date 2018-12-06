a8ace7edf9aa7bd05b842ce87d7e858767aa73e2
isabelamn07 <isabelamn07@users.noreply.github.com>
2018-12-06 20:42:50 -0200

    Update Coelho.java

--- New-Foxes-Rabbits/src/Coelho.java
+++ New-Foxes-Rabbits/src/Coelho.java
@@ -6,20 +6,25 @@ import java.util.List;
 
 public class Coelho extends Animal
 {
-    // Characteristics shared by all rabbits (class variables).
-
-    // The age at which a rabbit can start to breed.
+    // Características comuns aos coelhos(class variables).
+    // A idade que um coelho pode começar a procriar.
     private static int IDADE_REPRODUCAO = 5;
-    // The age to which a rabbit can live.
+    // A idade máxima de um coelho.
     private static int IDADE_MAXIMA = 40;
-    // The likelihood of a rabbit breeding.
+    // A probabilidade de reproduzir do coelho.
     private static double PROBABILIDADE_REPRODUCAO = 0.12;
-    // The maximum number of births.
+    // O número máximo de filhotes.
     private static int MAX_NASCIMENTO = 4;
-    // food value from grass
+    // Valor atribuído à alimentação do coelho - a grama
     private static final int VALOR_ALIMENTO_GRAMA = 6;
     
-    
+    /**
+     * Construtor de criação do coelho, que recebe se ele terá idade aleatória
+     * ou será um recém nascido e sua localizaõ no campo.
+     * @param idadeAleatoria valor booleano
+     * @param campo - tipo da Classe Campo
+     * @param localizacao - tipo da Classe localização
+     */
     public Coelho(boolean idadeAleatoria, Campo campo, Localizacao localizacao)
     {
         super(campo, localizacao);
@@ -29,35 +34,42 @@ public class Coelho extends Animal
         }
     }
       
-    
+    /**
+     * Configuração das ações do coelho, coelhos reproduzem, comem grama ou
+     * podem virar presa
+     * @param novosCoelhos 
+     */
     @Override
     public void act(List<Personagem> novosCoelhos)
     {
         envelhecer();
         if(ehVivo()) {
             darHaLuz(novosCoelhos);            
-            // Move towards a source of food if found.
+            // Se move para procurar comida
             Localizacao novaLocalizacao = procuraComida();
             if(novaLocalizacao == null) { 
-                // No food found - try to move to a free location.
+                // Quando não encontra comida, procura uma nova localização
                 novaLocalizacao = getCampo().LocalizacaoAdjLivre(getLocalizacao());
             }
-            // See if it was possible to move.
+            // Verifica se é possível se mover.
             if(novaLocalizacao != null) {
                 setLocalizacao(novaLocalizacao);
             }
             else {
-                // Overcrowding.
+                // Superpopulaçao de coelhos.
                 matar();
             }
         }
     }
-
+    /**
+     * Reprodução dos coelhos, coelhos recém-nascidos recebem idade igual a zero
+     * e são colocados dentro do campo em localições adjacentes de onde nasceram
+     * @param novosCoelhos 
+     */
     
     private void darHaLuz(List<Personagem> novosCoelhos)
     {
-        // New rabbits are born into adjacent locations.
-        // Get a list of adjacent free locations.
+        // Novos coelhos nascem em localizações adjacentes.
         Campo campo = getCampo();
         List<Localizacao> livre = campo.getLocalizacoesAdjLivres(getLocalizacao());
         int births = procriar();
@@ -67,7 +79,10 @@ public class Coelho extends Animal
             novosCoelhos.add(young);					
         }
     }
-
+    /**
+     * Procura por localizações vazias onde há grama.
+     * @return 
+     */
    
     private Localizacao procuraComida()
     {
@@ -89,7 +104,10 @@ public class Coelho extends Animal
         return null;
     }
     
-    
+    /**
+     * Determina se o coelho pode procriar ou não
+     * @return 
+     */
     @Override
     protected boolean podeProcriar()
     {
@@ -97,36 +115,48 @@ public class Coelho extends Animal
     }
     
     /**
-     * Getter om BREEDING_AGE op te halen
-     * @return 
+     * Retorna se o coelho está em idade de procriação
+     * @return IDADE_REPRODUÇÃO
      */
     protected int getIdadeReproducao()
     {
     	return IDADE_REPRODUCAO;
     }
     
-    
+    /**
+     * Retorna a idade limite do coelho
+     * @return IDADE_MAXIMA
+     */
     @Override
     protected int idadeMaxima()
     {
     	return IDADE_MAXIMA;
     }
     
-    
+    /**
+     * Retorna a probabiliade de procriar de um coelho
+     * @return PROBABILIDADE_REPRODUCAO
+     */
     @Override
     protected double getProbabilidadeProcriar()
     {
     	return PROBABILIDADE_REPRODUCAO;
     }
     
-    
+    /**
+     * Retorna o maior número de nascimentos possível
+     * @return MAX_NASCIMENTO
+     */
     @Override
     protected int getNumMaxNascimento()
     {
     	return MAX_NASCIMENTO;
     }
     
-
+    /**
+     * Configura a idade de reprodução de um coelho
+     * @param novaIdadeReproducao 
+     */
     
     public static void setIdadeReproducao(int novaIdadeReproducao)
     {
@@ -134,7 +164,7 @@ public class Coelho extends Animal
     }
     
     /**
-     *
+     * Configura a idade máxima de um coelho
      * @param novaIdade
      */
     public static void setIdadeMaxima(int novaIdade)
@@ -143,7 +173,7 @@ public class Coelho extends Animal
     }
     
     /**
-     *
+     * Configura a probabilidade de reprodução do coelho
      * @param novaProbReproucao
      */
     public static void serProbReproducao(double novaProbReproucao)
@@ -151,7 +181,10 @@ public class Coelho extends Animal
     	PROBABILIDADE_REPRODUCAO = novaProbReproucao;
     }
     
-    
+    /**
+    * Confira o número máximo de nascimentosd
+    * @param novaMaxNascimento
+    */
     public static void setMaxNascimento(int novaMaxNascimento)
     {
     	MAX_NASCIMENTO = novaMaxNascimento;
