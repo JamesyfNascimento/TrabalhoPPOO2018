adc34dcb9ac62a510c1774c138637774963cb7a1
isabelamn07 <isabelamn07@users.noreply.github.com>
2018-12-06 20:47:24 -0200

    Update Contador.java

--- New-Foxes-Rabbits/src/Contador.java
+++ New-Foxes-Rabbits/src/Contador.java
@@ -3,36 +3,50 @@ import java.awt.Color;
 
 public class Contador
 {
-    // A name for this type of simulation participant
+    // Um para nome para o tipo de partipante 
     private String name;
-    // How many of this type exist in the simulation.
+    // Contador do tipo especificado
     private int count;
 
-    
+    /**
+     * Construtor padrão que recebe uma string contendo o nome do "tipo" que 
+     * será contado
+     * @param name 
+     */
     public Contador(String name)
     {
         this.name = name;
         count = 0;
     }
-    
+    /**
+     * Retorna o nome especificado
+     * @return name
+     */
     public String getName()
     {
         return name;
     }
 
-    
+    /**
+     * Retorna o contador
+     * @return count
+     */
     public int getCount()
     {
         return count;
     }
 
-    
+    /**
+     * Incrementa o contador
+     */
     public void increment()
     {
         count++;
     }
     
-    
+    /**
+     * Zera o contador
+     */
     public void reset()
     {
         count = 0;
