fb25d5246243a5143c9eae6f0665995ed38e54c4
isabelamn07 <isabelamn07@users.noreply.github.com>
2018-12-06 21:21:20 -0200

    Update Personagem.java

--- New-Foxes-Rabbits/src/Personagem.java
+++ New-Foxes-Rabbits/src/Personagem.java
@@ -1,15 +1,28 @@
 
 import java.util.List;
 
-
+/**
+ * Interface para criar novos agentes que não são animais no campo no campo
+ * @author isabela
+ */
 public interface Personagem 
 {
-	
-
+        /**
+         * Ações do personagem
+         * @param newActors 
+         */
 	void act(List<Personagem> newActors);
         
+        /**
+         * Status de sua vida
+         * @return true or false
+         */
 	boolean ehVivo();
    
+        /**
+         * Atribuir localização
+         * @param newLocation 
+         */
 	void setLocalizacao(Localizacao newLocation);
   	
 }
