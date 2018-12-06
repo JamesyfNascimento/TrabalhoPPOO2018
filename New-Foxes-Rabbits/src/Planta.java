3abc7958b5ee539884762c277ea7e838ac3c060b
isabelamn07 <isabelamn07@users.noreply.github.com>
2018-12-06 21:37:49 -0200

    Update Planta.java

--- New-Foxes-Rabbits/src/Planta.java
+++ New-Foxes-Rabbits/src/Planta.java
@@ -1,42 +1,57 @@
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Classe abstrata para criação de seres vivos que façam parte da vegetação
+ * @author Anderson, Isabela, James
+ */
 public abstract class Planta implements Personagem {
-  // Whether the plant is alive or not.
+  // Se está viva ou não.
   private boolean vivo;
-  // The plant's field.
+  // Campo da planta.
   private Campo campo;
-  // The plant's position in the field.
+  // Localização.
   private Localizacao localizacao;
-  // An plant's age.
+  // Idade da planta.
   private int idade;
-  // An plant's food level, which is increased by eating.
+  // Nível da alimentação
   private int nivelAlimentar;
 
-  // The food value of a single plant. In effect, this is the
-  // number of steps an plant can go before it has to eat again.
+  
   public static final int NUM_PASSOS_REPRODUZIR = 12;
-  // A shared random number generator to control breeding.
+  // gerador de valores aleatórios
   protected static final Random rand = Randomico.getRandom();
 
- 
+ /**
+     * Construtor que recebe os parametros do campo da planta e a localizacao e 
+     * cria a planta na localizacao determinada por padrao viva (obvio).
+     * @param campo do tipo Campo
+     * @param localizacao do tipo Localização
+     */    
   public Planta(Campo campo, Localizacao localizacao) {
     vivo = true;
     this.campo = campo;
     setLocalizacao(localizacao);
   }
-
- 
+    /**
+     * Método para determinar a ação do personagem
+     * @param novasPlantas
+     */
   @Override
   public abstract void act(List<Personagem> novasPlantas);
 
-  
+  /**
+     * Retorna o status de vida da planta.
+     * @return true se a planta estiver vivo
+     */
   @Override
   public boolean ehVivo() {
     return vivo;
   }
 
- 
+    /**
+     * Ao ser ser declarada morta a planta é retirada do campo.
+     */ 
   protected void matar() {
     vivo = false;
     if (localizacao != null) {
@@ -45,7 +60,9 @@ public abstract class Planta implements Personagem {
       campo = null;
     }
   }
-
+  /**
+   * Aumenta a idade até chegar ao limite
+   */
   protected void envenhecer() {
     setIdade(getIdade() + 1);
     if (getIdade() > getIdadeMaxima()) {
@@ -53,15 +70,24 @@ public abstract class Planta implements Personagem {
     }
   }
 
-  
+  /**
+   * Para retornar a idade máxima
+   * @return 
+   */  
   protected abstract int getIdadeMaxima();
 
-  
+    /**
+     * Retorna a localizacao da planta.
+     * @return localização
+     */
   protected Localizacao getLocalizacao() {
     return localizacao;
   }
 
- 
+  /**
+   * Atribuição de nova localização
+   * @param newLocation 
+   */
   @Override
   public void setLocalizacao(Localizacao newLocation) {
     if (localizacao != null) {
@@ -71,12 +97,18 @@ public abstract class Planta implements Personagem {
     campo.lugar(this, newLocation);
   }
 
-  
+  /**
+   * Retorna o campo
+   * @return campo 
+   */
   protected Campo getCampo() {
     return campo;
   }
 
-  
+  /**
+   * Criar as novas gerações
+   * @return births
+   */
   protected int procriar() {
     int births = 0;
     if (podeNascer() && getRandom().nextDouble() <= getprobabilidadeNascer()) {
@@ -85,37 +117,61 @@ public abstract class Planta implements Personagem {
     return births;
   }
 
-  
+   /**
+   * Se pode nascer, gerar novos espécimes
+   * @return 
+   */
   protected abstract boolean podeNascer();
 
-  
+  /**
+   * Retorna idade
+   * @return idade 
+   */
   protected int getIdade() {
     return idade;
   }
 
-  
+  /**
+   * Define a idade
+   * @param age 
+   */
   protected void setIdade(int age) {
     this.idade = age;
   }
 
-  
+  /**
+   * Retorna nível de alimentação
+   * @return nivelAlimentar
+   */
   protected int getNivelAlimentar() {
     return nivelAlimentar;
   }
 
-  
+  /**
+   * Atribui nível alimentar
+   * @param nivelAlimentar 
+   */
   protected void setNivelAlimentar(int nivelAlimentar) {
     this.nivelAlimentar = nivelAlimentar;
   }
 
-  
+  /**
+   * Retorna valor aleatório
+   * @return rand
+   */  
   protected Random getRandom() {
     return rand;
   }
 
-  
+  /**
+   * Retornar máximo de nascimentos
+   * @return 
+   */
   protected abstract int getMaxNascimento();
 
-  
+  /**
+   * Retornar probabilidade de nascer
+   * @return 
+   */
   protected abstract double getprobabilidadeNascer();
 }
