78d47b07243cd23f49326e50f0825a882e4b50f2
isabelamn07 <isabelamn07@users.noreply.github.com>
2018-12-06 21:12:54 -0200

    Update HistoricoTela.java

--- New-Foxes-Rabbits/src/HistoricoTela.java
+++ New-Foxes-Rabbits/src/HistoricoTela.java
@@ -1,36 +1,44 @@
 import java.awt.Color;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+/**
+ * Classe que mostra as informaçẽos do está acontecendo no simulador
+ * @author Anderson, Isabela, James
+ */
 public class HistoricoTela extends JPanel
 {
-	//	text area
+	// Area de texto que mostra informações 
 	private JTextArea textArea;
 	private int step = -1;
-	
-	//	hashmap die hoeveelheid per kleur bij houdt
+
 	private HashMap<Color, Contador> stats;
-	
+	/**
+         * A Classe Historico tela está configurada para mostrar informações 
+         * sobre o número de seres vivos no ambiente.
+         * @param height
+         * @param width 
+         */
 	public HistoricoTela(int height, int width)
 	{
 		textArea = new JTextArea(height, width);
 	}
 	
-	
+	/**
+         * Recebe as informações
+         * @param stats 
+         */
 	public void stats(HashMap<Color, Contador> stats)
 	{
 		this.stats = stats;
 	}
 	
-	
+	/**
+         * A cada passo/iteração as informações sobre os números de seres no 
+         * campo é atualizado e mostrado na área de texto
+         */
 	public void historico()
 	{
             try {
@@ -45,79 +53,36 @@ public class HistoricoTela extends JPanel
                     buffer.append(info.getCount());
                     buffer.append(' ');
                 }
-                //	tijdelijk blokkeren
+                
                 if (!(nStep.equals("Step: " + -1 + "  "))){
-                    textArea.append(nStep + buffer.toString() + "\r\n");
-                    escreveEmArquivo(buffer.toString());
+                    textArea.append(nStep + buffer.toString() + "\r\n");                    
 
                 }
                 step++;
             } catch (Exception e) {
             }
-        }
-        
-        
-        public void escreveEmArquivo(String log) throws IOException{
-            FileWriter arquivo = null;
-            BufferedWriter bw = null;
-            try {
-                    Date data = new Date();
-                    SimpleDateFormat formatador = new SimpleDateFormat("EEE, MMM d");
-                    String dt = formatador.format(data).toString();
-                    StringBuffer buffer = new StringBuffer();
-                    buffer.append("log/log");
-                    buffer.append("-");
-                    buffer.append(dt);                   
-                    buffer.append(".txt");
-                    
-                    File file= new File(buffer.toString());
-                    
-                    if (!file.exists()) {
-                        file.createNewFile();
-                    }
-                    
-                    arquivo = new FileWriter(file.getAbsoluteFile(), true);
-                    
-                    bw = new BufferedWriter(arquivo);
-                    System.out.println(log);
-                    bw.write(log);
-                    bw.newLine();
-            } catch (IOException e) {
-                    e.printStackTrace();
-            } catch (Exception e) {
-                    e.printStackTrace();
-            }
-            finally{
-                try {
-
-                    if (bw != null)
-                            bw.close();
-
-                    if (arquivo != null)
-                            arquivo.close();
-
-                } catch (IOException ex) {
-
-                    ex.printStackTrace();
-
-                }
-
-
-            }
-        }
-	
-	
+        }	
+	/**
+         * Configuração dos passos/iterações
+         * @param step 
+         */
 	public void setStep(int step)
 	{
 		this.step = step;
 	}
-	
+	/**
+         * Configura a área de texto
+         * @param textArea 
+         */
 	public void setTextArea(JTextArea textArea)
 	{
 		this.textArea = textArea;
 	}
 	
-	
+	/**
+         * Retorna a área de texto
+         * @return textArea
+         */
 	public JTextArea getTextArea()
 	{
 		return textArea;
