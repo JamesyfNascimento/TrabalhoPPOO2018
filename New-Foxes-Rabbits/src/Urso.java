5520d997e52fa84a10fe5b5f6467ff3277bf25c9
isabelamn07 <isabelamn07@users.noreply.github.com>
2018-12-06 22:44:07 -0200

    Update Urso.java

--- New-Foxes-Rabbits/src/Urso.java
+++ New-Foxes-Rabbits/src/Urso.java
@@ -1,155 +1,212 @@
+
 import java.util.List;
 import java.util.Iterator;
 
 /**
- * A simple model of a Bear. Bear age, move, eat fox and rabbits, and die.
+ * Um modelo simples de um urso que pose se mover, comer ou morrer.
  */
 public class Urso extends Animal {
-  // Characteristics shared by all bears (class variables).
+    // Características compartilhadas entre os ursos.
 
-  // The age at which a bear can start to breed.
-  private static int IDADE_REPRODUCAO = 30;
-  // The age to which a bear can live.
-  private static int IDADE_MAXIMA = 190;
-  // The likelihood of a bear breeding.
-  private static double PROBABILIDADE_REPRODUCAO = 0.02;
-  // The maximum number of births.
-  private static int MAX_NASCIMENTO = 1;
-  // The food value of a single rabbit and a single fox. In effect, this is the
-  // number of steps a bear can go before it has to eat again.
-  private static final int NUM_PASSOS_REPRODUZIR = 9;
-  private static final int VALOR_ALIMENTO_RAPOSA = 18;
+    // A idade que um urso pode começar a procriar.
+    private static int IDADE_REPRODUCAO = 30;
+    // A idade máxim que pode viver o urso
+    private static int IDADE_MAXIMA = 190;
+    // Probabiliade de procriar
+    private static double PROBABILIDADE_REPRODUCAO = 0.02;
+    // Número máximo de filhotes.
+    private static int MAX_NASCIMENTO = 1;
+    // Valor da alimentação e quantidade de passos para comer novamente
+    private static final int NUM_PASSOS_REPRODUZIR = 9;
+    private static final int VALOR_ALIMENTO_RAPOSA = 18;
 
-  
-  public Urso(boolean idadeAleatoria, Campo campo, Localizacao localizacao) {
-    super(campo, localizacao);
-    if (idadeAleatoria) {
-      setIdade(getRandom().nextInt(IDADE_MAXIMA));
-      setNivelComida(getRandom().nextInt(NUM_PASSOS_REPRODUZIR + VALOR_ALIMENTO_RAPOSA));
-    } else {
-      setIdade(0);
-      setNivelComida(NUM_PASSOS_REPRODUZIR + VALOR_ALIMENTO_RAPOSA);
-    }
-  }
-
-  
-  @Override
-  public void act(List<Personagem> novoUrsos) {
-    envelhecer();
-    aumentarFome();
-    if (ehVivo()) {
-      darHaLuz(novoUrsos);
-      // Move towards a source of food if found.
-      Localizacao novaLocalizacao = procuraComida();
-      if (novaLocalizacao == null) {
-        // No food found - try to move to a free location.
-        novaLocalizacao = getCampo().LocalizacaoAdjLivre(getLocalizacao());
-      }
-      // See if it was possible to move.
-      if (novaLocalizacao != null) {
-        setLocalizacao(novaLocalizacao);
-      } else {
-        // Overcrowding.
-        matar();
-      }
-    }
-  }
-
- 
-  @Override
-  protected int idadeMaxima() {
-    return IDADE_MAXIMA;
-  }
-
-   
-  private void aumentarFome() {
-    setNivelComida(getNivelComida() - 1);
-    if (getNivelComida() <= 0) {
-      matar();
-    }
-  }
-
- 
-  private Localizacao procuraComida() {
-    Campo campo = getCampo();
-    List<Localizacao> adjacente = campo.localizacoesAdjacentes(getLocalizacao());
-    Iterator<Localizacao> it = adjacente.iterator();
-    while (it.hasNext()) {
-      Localizacao onde = it.next();
-      Object animal = campo.getPersonagem(onde);
-      if (animal instanceof Coelho) {
-        Coelho coelho = (Coelho) animal;
-        if (coelho.ehVivo()) {
-          coelho.matar();
-          setNivelComida(NUM_PASSOS_REPRODUZIR);
-          return onde;
+    /**
+     * Criação de um urso com idade aleatória ou recém-nascido
+     *
+     * @param idadeAleatoria
+     * @param campo
+     * @param localizacao
+     */
+    public Urso(boolean idadeAleatoria, Campo campo, Localizacao localizacao) {
+        super(campo, localizacao);
+        if (idadeAleatoria) {
+            setIdade(getRandom().nextInt(IDADE_MAXIMA));
+            setNivelComida(getRandom().nextInt(NUM_PASSOS_REPRODUZIR + VALOR_ALIMENTO_RAPOSA));
+        } else {
+            setIdade(0);
+            setNivelComida(NUM_PASSOS_REPRODUZIR + VALOR_ALIMENTO_RAPOSA);
         }
-      } else if (animal instanceof Raposa) {
-        Raposa raposa = (Raposa) animal;
-        if (raposa.ehVivo()) {
-          raposa.matar();
-          setNivelComida(VALOR_ALIMENTO_RAPOSA);
-          return onde;
+    }
+
+    /**
+     * Ações do urso
+     *
+     * @param novoUrsos
+     */
+    @Override
+    public void act(List<Personagem> novoUrsos) {
+        envelhecer();
+        aumentarFome();
+        if (ehVivo()) {
+            darHaLuz(novoUrsos);
+            // Procura comida
+            Localizacao novaLocalizacao = procuraComida();
+            if (novaLocalizacao == null) {
+                // Se não acha comida se move.
+                novaLocalizacao = getCampo().LocalizacaoAdjLivre(getLocalizacao());
+            }
+            // Verifica se é possível se mover
+            if (novaLocalizacao != null) {
+                setLocalizacao(novaLocalizacao);
+            } else {
+                // Superpopulação.
+                matar();
+            }
         }
-
-      }
     }
-    return null;
-  }
 
-  
-  private void darHaLuz(List<Personagem> novosUrsos) {
-    // New bears are born into adjacent locations.
-    // Get a list of adjacent free locations.
-    Campo campo = getCampo();
-    List<Localizacao> livre = campo.getLocalizacoesAdjLivres(getLocalizacao());
-    int births = procriar();
-    for (int b = 0; b < births && livre.size() > 0; b++) {
-      Localizacao loc = livre.remove(0);
-      Urso filhote = new Urso(false, campo, loc);
-      novosUrsos.add(filhote);
+    /**
+     * Idade máxima
+     *
+     * @return IDADE_MAXIMA
+     */
+    @Override
+    protected int idadeMaxima() {
+        return IDADE_MAXIMA;
     }
-  }
 
-  
-  @Override
-  protected boolean podeProcriar() {
-    return getIdade() >= getIdadeReproduzir();
-  }
+    /**
+     * Diminui o nivel de comida e aumenta a fome
+     */
+    private void aumentarFome() {
+        setNivelComida(getNivelComida() - 1);
+        if (getNivelComida() <= 0) {
+            matar();
+        }
+    }
 
-  protected int getIdadeReproduzir() {
-    return IDADE_REPRODUCAO;
-  }
+    /**
+     * Ações necessárias para procurar comida
+     *
+     * @return
+     */
+    private Localizacao procuraComida() {
+        Campo campo = getCampo();
+        List<Localizacao> adjacente = campo.localizacoesAdjacentes(getLocalizacao());
+        Iterator<Localizacao> it = adjacente.iterator();
+        while (it.hasNext()) {
+            Localizacao onde = it.next();
+            Object animal = campo.getPersonagem(onde);
+            if (animal instanceof Coelho) {
+                Coelho coelho = (Coelho) animal;
+                if (coelho.ehVivo()) {
+                    coelho.matar();
+                    setNivelComida(NUM_PASSOS_REPRODUZIR);
+                    return onde;
+                }
+            } else if (animal instanceof Raposa) {
+                Raposa raposa = (Raposa) animal;
+                if (raposa.ehVivo()) {
+                    raposa.matar();
+                    setNivelComida(VALOR_ALIMENTO_RAPOSA);
+                    return onde;
+                }
 
-  
-  @Override
-  protected int getNumMaxNascimento() {
-    return MAX_NASCIMENTO;
-  }
+            }
+        }
+        return null;
+    }
 
- 
-  @Override
-  protected double getProbabilidadeProcriar() {
-    return PROBABILIDADE_REPRODUCAO;
-  }
+    /**
+     * Reproduzir
+     *
+     * @param novosUrsos
+     */
+    private void darHaLuz(List<Personagem> novosUrsos) {
+        // New bears are born into adjacent locations.
+        // Get a list of adjacent free locations.
+        Campo campo = getCampo();
+        List<Localizacao> livre = campo.getLocalizacoesAdjLivres(getLocalizacao());
+        int births = procriar();
+        for (int b = 0; b < births && livre.size() > 0; b++) {
+            Localizacao loc = livre.remove(0);
+            Urso filhote = new Urso(false, campo, loc);
+            novosUrsos.add(filhote);
+        }
+    }
 
- 
-  public static void setIdadeReproducao(int novaIdadeReproducao) {
-    IDADE_REPRODUCAO = novaIdadeReproducao;
-  }
+    /**
+     * Determina se o urso pode procriar ou não
+     *
+     * @return getIdade() >= getIdadeReproduzir()
+     */
+    @Override
+    protected boolean podeProcriar() {
+        return getIdade() >= getIdadeReproduzir();
+    }
 
-  
-  public static void setIdadeMaxima(int novaIdadeMaxima) {
-    IDADE_MAXIMA = novaIdadeMaxima;
-  }
+    /**
+     * Retorna se o urso está em idade de procriação
+     *
+     * @return IDADE_REPRODUÇÃO
+     */
+    protected int getIdadeReproduzir() {
+        return IDADE_REPRODUCAO;
+    }
 
-  
-  public static void setProbReproducao(double novaProbReproducao) {
-    PROBABILIDADE_REPRODUCAO = novaProbReproducao;
-  }
+    /**
+     * Retorna o maior número de nascimentos possível
+     *
+     * @return MAX_NASCIMENTO
+     */
+    @Override
+    protected int getNumMaxNascimento() {
+        return MAX_NASCIMENTO;
+    }
 
- 
-  public static void setMaxNascimento(int maxNascimento) {
-    MAX_NASCIMENTO = maxNascimento;
-  }
+    /**
+     * Retorna a probabiliade de procriar.
+     *
+     * @return PROBABILIDADE_REPRODUCAO
+     */
+    @Override
+    protected double getProbabilidadeProcriar() {
+        return PROBABILIDADE_REPRODUCAO;
+    }
+
+    /**
+     * Configura a idade de reprodução de um urso
+     *
+     * @param novaIdadeReproducao
+     */
+    public static void setIdadeReproducao(int novaIdadeReproducao) {
+        IDADE_REPRODUCAO = novaIdadeReproducao;
+    }
+
+    /**
+     * Configura a idade máxima de uma raposa
+     *
+     * @param novaIdadeMaxima
+     */
+    public static void setIdadeMaxima(int novaIdadeMaxima) {
+        IDADE_MAXIMA = novaIdadeMaxima;
+    }
+
+    /**
+     * Atriubir nova probalidade de reprodução
+     *
+     * @param novaProbReproducao
+     */
+    public static void setProbReproducao(double novaProbReproducao) {
+        PROBABILIDADE_REPRODUCAO = novaProbReproducao;
+    }
+
+    /**
+     * Configura o número máximo de nascimentos
+     *
+     * @param maxNascimento
+     */
+    public static void setMaxNascimento(int maxNascimento) {
+        MAX_NASCIMENTO = maxNascimento;
+    }
 }
