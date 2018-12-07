e76dbef3dc0bc47cbc580a863c8b345c0679c315
isabelamn07 <isabelamn07@users.noreply.github.com>
2018-12-07 05:53:42 -0200

    Update Coelho.java

--- New-Foxes-Rabbits/src/Coelho.java
+++ New-Foxes-Rabbits/src/Coelho.java
@@ -4,7 +4,9 @@ import java.util.List;
 /**
  * Classe para criacao de um simples coelho que pode procriar, comer grama, 
  * servir de presa para outros animais e morrer
+ * @author isabela
  */
+
 public class Coelho extends Animal
 {
     // Caracteristicas comuns aos coelhos(class variables).
