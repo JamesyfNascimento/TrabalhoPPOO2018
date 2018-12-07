1d55bc25e08ee90b8f832025bafdc406ee8c498d
isabelamn07 <isabelamn07@users.noreply.github.com>
2018-12-07 05:58:36 -0200

    Update Principal.java

--- New-Foxes-Rabbits/src/Principal.java
+++ New-Foxes-Rabbits/src/Principal.java
@@ -6,7 +6,11 @@ public class Principal {
 
     private static Simulador simulador;
 
-    
+    /**
+     * Classe main apenas inicializa o simulador
+     * @param args
+     * @throws Exception 
+     */
     public static void main(String[] args) throws Exception {
 
 
@@ -14,12 +18,18 @@ public class Principal {
         
     }
 
-   
+    /**
+     * Retorna o simulador
+     * @return simulador
+     */
     public static Simulador getSimulador() {
         return simulador;
     }
 
-    
+    /**
+     * Modifica a configuracao do simulador
+     * @param simulador 
+     */
     public static void setSimulador(Simulador simulador) {
         Principal.simulador = simulador;
     }
