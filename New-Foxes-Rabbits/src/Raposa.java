9b6ea5c39f34e1b6c695ff609a6bf42af61b6e93
isabelamn07 <isabelamn07@users.noreply.github.com>
2018-12-06 22:03:58 -0200

    Update Raposa.java

--- New-Foxes-Rabbits/src/Raposa.java
+++ New-Foxes-Rabbits/src/Raposa.java
@@ -1,59 +1,73 @@
 import java.util.List;
 import java.util.Iterator;
 
-public class Raposa extends Animal {
-    // Characteristics shared by all foxes (class variables).
 
-    // The age at which a fox can start to breed.
+/**
+ * Classe com as caracerísticas e ações das raposas
+ * @author Anderson, Isabela, James
+ */
+public class Raposa extends Animal {
+     // Características comuns às raposas.
+    // A idade que uma raposa pode começar a procriar.
     private static int IDADE_REPRODUCAO = 15;
-    // The age to which a fox can live.
+    // Idade máxima que uma raposa pode viver.
     private static int IDADE_MAXIMA = 150;
-    // The likelihood of a fox breeding.
+    // Probabilidade da raposa procriar
     private static double PROBABILIDADE_REPRODUCAO = 0.08;
-    // The maximum number of births.
+     // Máximo de filhores
     private static int MAX_NASCIMENTO = 4;
-    // The food value of a single rabbit. In effect, this is the
-    // number of steps a fox can go before it has to eat again.
-    private static final int NUM_PASSOS_REPRODUZIR = 9;
+    // Número de passo até poder comer de novo
+    private static final int NIVEL_COMIDA = 9;
 
     public Raposa(boolean idadeAleatoria, Campo campo, Localizacao localizacao) {
         super(campo, localizacao);
         if (idadeAleatoria) {
             setIdade(getRandom().nextInt(IDADE_MAXIMA));
-            setNivelComida(getRandom().nextInt(NUM_PASSOS_REPRODUZIR));
+            setNivelComida(getRandom().nextInt(NIVEL_COMIDA));
         } else {
             setIdade(0);
-            setNivelComida(NUM_PASSOS_REPRODUZIR);
+            setNivelComida(NIVEL_COMIDA);
         }
     }
-
+    /**
+     * Ações de uma raposa (comer, se mover)
+     * @param novasRaposas 
+     */
     @Override
     public void act(List<Personagem> novasRaposas) {
         envelhecer();
         aumentaFome();
         if (ehVivo()) {
             darHaLuz(novasRaposas);
-            // Move towards a source of food if found.
+            // Se move para procurar comida
             Localizacao novaLocalizacao = procuraComida();
             if (novaLocalizacao == null) {
-                // No food found - try to move to a free location.
+                // Se não encontrar comida, tenta achar uma nova localização 
+                // vazia
                 novaLocalizacao = getCampo().LocalizacaoAdjLivre(getLocalizacao());
             }
             // See if it was possible to move.
             if (novaLocalizacao != null) {
                 setLocalizacao(novaLocalizacao);
             } else {
-                // Overcrowding.
+                // Superpopulação.
                 matar();
             }
         }
     }
 
+    /**
+     * Retorna a idade máxima
+     * @return IDADE_MAXIMA
+     */
     @Override
     protected int idadeMaxima() {
         return IDADE_MAXIMA;
     }
 
+    /**
+     * Aumentar fome diminuindo o nível de comida
+     */
     private void aumentaFome() {
         setNivelComida(getNivelComida() - 1);
         if (getNivelComida() <= 0) {
@@ -61,6 +75,10 @@ public class Raposa extends Animal {
         }
     }
 
+    /**
+     * Procurar comida
+     * @return null, ou local onde há comida
+     */
     private Localizacao procuraComida() {
         Campo campo = getCampo();
         List<Localizacao> adjacente = campo.localizacoesAdjacentes(getLocalizacao());
@@ -72,7 +90,7 @@ public class Raposa extends Animal {
                 Coelho coelho = (Coelho) animal;
                 if (coelho.ehVivo()) {
                     coelho.matar();
-                    setNivelComida(NUM_PASSOS_REPRODUZIR);
+                    setNivelComida(NIVEL_COMIDA);
                     return onde;
                 }
             }
@@ -80,6 +98,11 @@ public class Raposa extends Animal {
         return null;
     }
 
+    /**
+     * Reprodução das raposas, raposas recém-nascidas recebem idade igual a zero
+     * e são colocados dentro do campo em localições adjacentes de onde nasceram
+     * @param novosCoelhos 
+     */
     private void darHaLuz(List<Personagem> novasRaposas) {
         // New foxes are born into adjacent locations.
         // Get a list of adjacent free locations.
@@ -93,37 +116,69 @@ public class Raposa extends Animal {
         }
     }
 
+    /**
+     * Determina se a raposa pode procriar ou não
+     * @return 
+     */
     @Override
     protected boolean podeProcriar() {
         return getIdade() >= getIdadeReproducao();
     }
 
+    /**
+     * Retorna se a raposa está em idade de procriação
+     * @return IDADE_REPRODUÇÃO
+     */
     protected int getIdadeReproducao() {
         return IDADE_REPRODUCAO;
     }
 
+    /**
+     * Retorna o maior número de nascimentos possível
+     * @return MAX_NASCIMENTO
+     */
     @Override
     protected int getNumMaxNascimento() {
         return MAX_NASCIMENTO;
     }
 
+        /**
+     * Retorna a probabiliade de procriar.
+     * @return PROBABILIDADE_REPRODUCAO
+     */
     @Override
     protected double getProbabilidadeProcriar() {
         return PROBABILIDADE_REPRODUCAO;
     }
 
+    /**
+     * Configura a idade de reprodução de um coelho
+     * @param novaIdadeReproducao 
+     */
     public static void setIdadeReproducao(int novaIdade) {
         IDADE_REPRODUCAO = novaIdade;
     }
 
+    /**
+     * Configura a idade máxima de um coelho
+     * @param novaIdade
+     */
     public static void setIdadeMaxima(int novaIdade) {
         IDADE_MAXIMA = novaIdade;
     }
 
+    /**
+     * Atriubir nova probalidade de reprodução
+     * @param prob 
+     */
     public static void setProbabilidadeReproducao(double prob) {
         PROBABILIDADE_REPRODUCAO = prob;
     }
 
+    /**
+    * Configura o número máximo de nascimentosd
+    * @param novaMaxNascimento
+    */
     public static void setMaxNascimento(int maxNascimento) {
         MAX_NASCIMENTO = maxNascimento;
     }
